ca2e8fde02b55536a623ee35e2b0b9818a0a70e5
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2023-04-17 20:42:33 +0200

    Add signIn/signUp modal

--- app/app-header.tsx
+++ app/app-header.tsx
@@ -1,13 +1,58 @@
+'use client';
+
+import { Auth } from "@supabase/auth-ui-react";
+import { useState } from "react";
+import { useSupabase } from "./supabase-provider";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
 export default function AppHeader() {
+  const { supabase } = useSupabase();
+
+  const [showAuth, setShowAuth] = useState(false)
+
+  const handleButtonClick = () => {
+    setShowAuth(true)
+  }
+
+  const handleCloseAuth = () => {
+    setShowAuth(false)
+  }
+  
   return(
     <div className="navbar bg-base-100">
       <div className="navbar-start items-start flex">
         <a className="btn btn-ghost normal-case text-xl">blooptwang</a>
       </div>
       <div className="navbar-end items-end flex gap-3">
-        <button className="btn btn-outline">Log in</button>
-        <button className="btn btn-outline">Sign up</button>
+        <label htmlFor="sign-up-modal" className="btn btn-outline">Sign up</label>
+        <label htmlFor="sign-in-modal" className="btn btn-outline">Sign in</label>
       </div>
+
+      <input type="checkbox" id="sign-up-modal" className="modal-toggle" />
+      <label htmlFor="sign-up-modal" className="modal cursor-pointer">
+        <label className="modal-box relative" htmlFor="">
+          <Auth
+            appearance={{ theme: ThemeSupa }}
+            providers={['facebook', 'github', 'google']}
+            supabaseClient={supabase}
+            theme="dark"
+            view="sign_up"
+          />
+        </label>
+      </label>
+
+      <input type="checkbox" id="sign-in-modal" className="modal-toggle" />
+      <label htmlFor="sign-in-modal" className="modal cursor-pointer">
+        <label className="modal-box relative" htmlFor="">
+          <Auth
+            appearance={{ theme: ThemeSupa }}
+            providers={['facebook', 'github', 'google']}
+            supabaseClient={supabase}
+            theme="dark"
+            view="sign_in"
+          />
+        </label>
+      </label>
     </div>
   )
 }
