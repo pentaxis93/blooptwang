56ed79d4b19280fa74fca5e8c9f3149c77e53286
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2023-04-18 14:02:29 +0200

    Implement supabase auth

--- app/app-header.tsx
+++ app/app-header.tsx
@@ -1,31 +1,46 @@
 'use client';
 
 import { Auth } from "@supabase/auth-ui-react";
-import { useState } from "react";
 import { useSupabase } from "./supabase-provider";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useUser, useSession } from "@supabase/auth-helpers-react";
 
 export default function AppHeader() {
   const { supabase } = useSupabase();
 
-  const [showAuth, setShowAuth] = useState(false)
+  const session = useSession();
 
-  const handleButtonClick = () => {
-    setShowAuth(true)
-  }
+  const signOut = () => {
+    supabase.auth.signOut();
+  };
 
-  const handleCloseAuth = () => {
-    setShowAuth(false)
-  }
-  
+  // Set redirect URL depending on environment
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      'http://localhost:3000/';
+    // Make sure to include `https://` when not localhost.
+    url = url.includes('http') ? url : `https://${url}`;
+    // Make sure to including trailing `/`.
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+    return url;
+  };
+
   return(
     <div className="navbar bg-base-100">
       <div className="navbar-start items-start flex">
         <a className="btn btn-ghost normal-case text-xl">blooptwang</a>
       </div>
       <div className="navbar-end items-end flex gap-3">
-        <label htmlFor="sign-up-modal" className="btn btn-outline">Sign up</label>
-        <label htmlFor="sign-in-modal" className="btn btn-outline">Sign in</label>
+        {!session ? (
+          <div className="flex gap-3">
+            <label htmlFor="sign-up-modal" className="btn btn-outline">Sign up</label>
+            <label htmlFor="sign-in-modal" className="btn btn-outline">Sign in</label>
+          </div>
+        ) : (
+            <button onClick={signOut} className="btn btn-outline">Sign out</button>
+        )}
       </div>
 
       <input type="checkbox" id="sign-up-modal" className="modal-toggle" />
@@ -33,10 +48,11 @@ export default function AppHeader() {
         <label className="modal-box relative" htmlFor="">
           <Auth
             appearance={{ theme: ThemeSupa }}
-            providers={['facebook', 'github', 'google']}
+            providers={['github']}
             supabaseClient={supabase}
             theme="dark"
             view="sign_up"
+            redirectTo={getURL()}
           />
         </label>
       </label>
@@ -46,10 +62,11 @@ export default function AppHeader() {
         <label className="modal-box relative" htmlFor="">
           <Auth
             appearance={{ theme: ThemeSupa }}
-            providers={['facebook', 'github', 'google']}
+            providers={['github']}
             supabaseClient={supabase}
             theme="dark"
             view="sign_in"
+            redirectTo={getURL()}
           />
         </label>
       </label>
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,22 @@
 'use client';
 
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import SupabaseProvider from "./supabase-provider";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
 export default function Providers({
   children
 }: {
   children: React.ReactNode
 }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
   return (
     <SupabaseProvider>
-      {children}
+      <SessionContextProvider supabaseClient={supabase} >
+        {children}
+      </SessionContextProvider>
     </SupabaseProvider>
   );
 }
