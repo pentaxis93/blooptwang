4a40b48a1bc614c400cdeedeea21151ea5792116
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2023-04-19 20:38:23 +0200

    Set up basic UI

---
+++ components/chat-footer.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+
+export default function ChatFooter() {
+  return (
+    <div className='w-9/12'>
+      <div className='input-group w-full'>
+        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+        <button className='btn btn-square'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
+            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ components/chat-panel.tsx
@@ -0,0 +1,10 @@
+import { headers, cookies } from "next/headers";
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+
+export default function ChatPanel() {
+  return (
+    <div className="flex flex-col align-middle items-center">
+
+    </div>
+  );
+};
