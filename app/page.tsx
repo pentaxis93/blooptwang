5af4713dca9eb6afc58a539358cd17e2ee390981
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2023-04-21 14:46:45 +0200

    Add skeleton UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AppHeader from './app-header';
 import { headers, cookies } from 'next/headers';
 import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import ChatPanel from '@/components/chat-panel';
-import ChatFooter from '@/components/chat-footer';
 
 export default async function Bots() {
   const supabase = createServerComponentSupabaseClient({
@@ -15,14 +14,11 @@ export default async function Bots() {
   return (
     <div className='h-screen w-screen flex flex-col align-middle items-center gap-3'>
       <AppHeader />
-      <div className='grow'>
-        <pre>{JSON.stringify(bots, null, 2)}</pre>
-      </div>
-      <div className='grow'>
+      {/* <div className='grow'> */}
+      {/*   <pre>{JSON.stringify(bots, null, 2)}</pre> */}
+      {/* </div> */}
+      <div className='flex flex-col w-screen grow bg-pink-200'>
         <ChatPanel />
-      </div>
-      <div className='grow-0'>
-        <ChatFooter />
       </div>
     </div>
   );
--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -1,10 +1,22 @@
-import { headers, cookies } from "next/headers";
-import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+'use client';
+
+import { useState } from 'react';
 
 export default function ChatPanel() {
-  return (
-    <div className="flex flex-col align-middle items-center">
+  // Define and reset state variables
+  const [prompt, setPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState("");
 
+  return (
+    <div className="flex flex-col align-middle h-full p-5 items-center">
+      <div id="chat-output" className="bg-green-200 grow">
+        Hello
+      </div>
+      <div id="chat-input" className='flex flex-col w-3/5 grow-0 gap-3 bg-blue-200'>
+        <textarea className="textarea textarea-primary" placeholder="Type here"></textarea>
+        <button className="btn btn-primary">Chat</button>
+      </div>
     </div>
   );
 };
