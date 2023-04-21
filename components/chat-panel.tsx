0edd3fa78ddf0f240c344f73d487bad7b6fbc9bb
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2023-04-21 17:48:34 +0200

    Activate and connect UI

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -1,22 +1,55 @@
 'use client';
 
+import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 
+type FormValues = {
+  userInput: string;
+};
+
 export default function ChatPanel() {
-  // Define and reset state variables
-  const [prompt, setPrompt] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [result, setResult] = useState("");
+  const {
+    register,
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      userInput: ""
+    }
+  });
+
+  let [reply, setReply] = useState("Hello, I'm a chatbot. Ask me a question!");
+
+  // const onSubmit = data => console.log(data);
+  const onSubmit = (data: FormValues) => {
+    fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: data.userInput,
+      }),
+    })
+      .then((res) => res.text())
+      .then((text) => {
+        console.log(text);
+        setReply(text);
+      });
+  };
 
   return (
     <div className="flex flex-col align-middle h-full p-5 items-center">
       <div id="chat-output" className="bg-green-200 grow">
-        Hello
+        <p>{reply}</p>
       </div>
-      <div id="chat-input" className='flex flex-col w-3/5 grow-0 gap-3 bg-blue-200'>
-        <textarea className="textarea textarea-primary" placeholder="Type here"></textarea>
-        <button className="btn btn-primary">Chat</button>
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col w-3/5 grow-0 gap-3 bg-blue-200'>
+        <textarea
+          {...register("userInput", { required: true })}
+          className="textarea textarea-primary"
+          placeholder="Type here">
+        </textarea>
+        <input type="submit" value="Chat" className="btn btn-primary" />
+      </form>
     </div>
   );
 };
